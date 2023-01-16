2ae68ec95cc602cb444477e0d66057dbbc193b18
Chad Condon <chad@condon.tech>
2023-01-16 11:27:11 -0800

    remove unspecified behavior tests

--- include/CppUTest/CppUTestConfig.h
+++ include/CppUTest/CppUTestConfig.h
@@ -130,39 +130,6 @@
 #endif
 
 /*
- * Address sanitizer is a good thing... and it causes some conflicts with the CppUTest tests
- * To check whether it is on or off, we create a CppUTest define here.
-*/
-#if defined(__has_feature)
-#if __has_feature(address_sanitizer)
-#define CPPUTEST_SANITIZE_ADDRESS 1
-#endif
-#endif
-
-#ifdef __SANITIZE_ADDRESS__
-#define CPPUTEST_SANITIZE_ADDRESS 1
-#endif
-
-#ifndef CPPUTEST_SANITIZE_ADDRESS
-#define CPPUTEST_SANITIZE_ADDRESS 0
-#endif
-
-#if CPPUTEST_SANITIZE_ADDRESS
-#define CPPUTEST_SANITIZE_ADDRESS 1
-#define CPPUTEST_DO_NOT_SANITIZE_ADDRESS __attribute__((no_sanitize_address))
-  #if defined(__linux__) && defined(__clang__)
-    #if CPPUTEST_USE_MEM_LEAK_DETECTION
-    #warning Compiling with Address Sanitizer with clang on linux will cause duplicate symbols for operator new. Turning off memory leak detection. Compile with -DCPPUTEST_MEM_LEAK_DETECTION_DISABLED to get rid of this warning.
-    #undef CPPUTEST_USE_MEM_LEAK_DETECTION
-    #define CPPUTEST_USE_MEM_LEAK_DETECTION 0
-    #endif
-  #endif
-#else
-#define CPPUTEST_SANITIZER_ADDRESS 0
-#define CPPUTEST_DO_NOT_SANITIZE_ADDRESS
-#endif
-
-/*
  * Handling of IEEE754 (IEC559) floating point exceptions via fenv.h
  * Predominantly works on non-Visual C++ compilers and Visual C++ 2008 and newer
  */
