3159f7123e08389551bf8c6481b9b92142129859
Chad Condon <chad@condon.tech>
2026-03-02 22:01:48 -0800

    Move package/install template files into pkg/
    Consolidate all packaging-related template files into a top-level pkg/
    directory, replacing the cmake/package/ directory introduced in the
    previous commit:
    
    - pkg/CppUTestConfig.cmake.install.in
    - pkg/CppUTestConfig.cmake.build.in
    - pkg/CppUTestConfig.cmake.autotools.in
    - pkg/CppUTestTargets.cmake.autotools.in
    - pkg/CppUTestTargets-relwithdebinfo.cmake.autotools.in
    - pkg/CppUTestConfigVersion.cmake
    - pkg/cpputest.pc.in
    
    Using a generic pkg/ directory rather than cmake/package/ better reflects
    that the directory covers multiple packaging systems (cmake and pkg-config).
    
    Update cmake/install.cmake, configure.ac, and Makefile.am accordingly.

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -9,7 +9,7 @@ set(libdir "\${exec_prefix}/${LIB_INSTALL_DIR}")
 set(includedir "\${prefix}/${INCLUDE_INSTALL_DIR}")
 set(PACKAGE_VERSION "${PROJECT_VERSION}")
 
-configure_file(cpputest.pc.in cpputest.pc @ONLY)
+configure_file(${PROJECT_SOURCE_DIR}/pkg/cpputest.pc.in cpputest.pc @ONLY)
 install(
     FILES "${CMAKE_CURRENT_BINARY_DIR}/cpputest.pc"
     DESTINATION ${LIB_INSTALL_DIR}/pkgconfig
@@ -19,7 +19,7 @@ include(CMakePackageConfigHelpers)
 
 set(CPPUTEST_CONFIG_DEST "${LIB_INSTALL_DIR}/CppUTest/cmake")
 
-configure_package_config_file(${CMAKE_CURRENT_LIST_DIR}/package/CppUTestConfig.cmake.install.in
+configure_package_config_file(${PROJECT_SOURCE_DIR}/pkg/CppUTestConfig.cmake.install.in
     ${CMAKE_CURRENT_BINARY_DIR}/install/CppUTestConfig.cmake
     INSTALL_DESTINATION "${CPPUTEST_CONFIG_DEST}"
     PATH_VARS INCLUDE_INSTALL_DIR LIB_INSTALL_DIR)
@@ -49,7 +49,7 @@ install(
         ${CMAKE_CURRENT_SOURCE_DIR}/cmake/Modules/_CppUTestDiscovery.cmake
     DESTINATION "${CPPUTEST_CONFIG_DEST}/Modules"
 )
-configure_package_config_file(${CMAKE_CURRENT_LIST_DIR}/package/CppUTestConfig.cmake.build.in
+configure_package_config_file(${PROJECT_SOURCE_DIR}/pkg/CppUTestConfig.cmake.build.in
     ${CMAKE_CURRENT_BINARY_DIR}/CppUTestConfig.cmake
     INSTALL_DESTINATION ${CMAKE_CURRENT_BINARY_DIR}
     PATH_VARS INCLUDE_DIR CMAKE_CURRENT_BINARY_DIR
